1d3226bd6e0a2341cc6ea1ea153294d749d53bf8
johnjoker27 <aminuabdulhameed27@gmail.com>
2026-03-27 21:51:28 +0000

    Update revisionl.c

--- revisionl.c
+++ revisionl.c
@@ -91,3 +91,168 @@ int*larger(int*x,int*y)//defining function
         return y;//return y
 }
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*void swap(int *n,int *b);
+
+int main()
+{
+    //pointers are variables that store the address of other variables.
+    int a = 25;
+    int *m;
+    m = &a;
+    //pointer assigning rules states that: the data type should be the same as that of the variable whose address it holds.
+    int e = 57;
+    void*z=&e;
+    //deferencing is accessing the value of the function whose pointer was made.
+    int x = 32;
+    int *r=&x;
+    //passing by referece(passing pointers to functions)
+    printf("before swapping:\n");
+    printf("a=%d\n",a);
+    printf("e=%d\n",e);
+
+    swap(&a,&e);//passing pointers to functions
+
+    printf("after swapping:\n");
+    printf("m=%d\n",a);
+    printf("a=%d",e);
+
+    return 0;
+}
+
+void swap(int *n,int *b)
+{
+    int temp;
+    temp = *n;
+    *n=*b;
+    *b=temp;
+}
+*/
+
+/*void func1(int u,int v);
+void func2(int*pu,int*pv);
+
+int main()
+{
+    int u = 1;
+    int v = 2;
+
+    printf("\nBefore calling func1:u=%d,v=%d\n",u,v);
+    func1(u,v);
+    printf("\nAfter calling func1:u=%d,v=%d\n",u,v);
+
+    printf("\nBefore calling func2:u=%d,v=%d\n",u,v);
+    func2(&u,&v);
+    printf("\nAfter calling func2:u=%d,v=%d\n",u,v);
+
+    return 0;
+}
+
+void func1(int u,int v)
+{
+    u=0;
+    v=0;
+    printf("\nWithin func1u=%d,v=%d",u,v);
+
+}
+
+void func2(int *pu,int *pv)
+{
+    *pu=0;
+    *pv=0;
+    printf("Within func2:*pu=%d,*pv=%d",*pu,*pv);
+
+}*/
+/*
+int*larger(int*,int*);//function declaration
+int main()
+{
+    int a = 15;//declaring and initializing a
+    int b = 92;//declaring and initializing b
+    int*p;//declaring the pointer p
+    p = larger(&a,&b);//assigning p to the address of larger value
+    printf("%d is larger",*p);//printing the value pointed to by p
+    return 0;
+}
+int*larger(int*x,int*y)//defining function
+{
+    if(*x>*y)//if pointer x value is greater than that of y
+        return x;//return x
+    else
+        return y;//return y
+}*/
+
+//declaring strings
+/*int main ()
+{
+    char city[] = "Kumasi";
+    char city[7] = "kumasi";
+    printf("%s is the city we live in.\n",city);
+    return 0;
+
+}*/
+
+/*int main()
+{
+    char fullname[250];
+    printf("Enter youor full name\n");
+    scanf("%s",&fullname);
+
+    printf("%s\n",fullname);
+    return 0;
+}*/
+/*
+int main()
+{
+    char fullname[250];
+    printf("enter your full name\n");
+    gets(fullname);
+    puts(fullname);
+
+    return 0;
+}*/
+/*
+int *ab;
+float *k;
+char *y;
+double *z;
+
+
+int main()
+{
+    int q = 55;
+    int *m;
+    m = &q;
+
+    return 0;
+}*/
+/*
+int main()
+{
+    int x = 9;
+    int *p = &x;
+
+    *p = 20;
+    printf("%d",*p);
+    return 0;
+
+}*/
+
+/*int main()
+{
+    int arr[3] = {10,20,30};
+    int *p = arr;
+
+    printf("%d\ns",*p);
+    printf("%d",*(p+1));
+    return 0;
+}*/
+
+
+
+
+
+
